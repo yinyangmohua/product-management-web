3cd6ea3268982a1de1d36cf8ba2499f0e524e388
yinyangmohua <3259296522@qq.com>
2024-10-29 15:35:09 +0800

    axios综合练习

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ProductManagement from "./components/ProductManagement.vue";
+</script>
+
+<template>
+  <ProductManagement />
+</template>
---
+++ src/api/productService.ts
@@ -0,0 +1,31 @@
+// src/api/productsService.ts
+import apiInstance from './axiosInstance';
+
+export interface Product {
+  id?: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  cover: string;
+}
+
+const productService = {
+  getProducts() {
+    return apiInstance.get<Product[]>('/products');
+  },
+  getProductById(id: number) {
+    return apiInstance.get<Product>(`/products/${id}`);
+  },
+  createProduct(data: Product) {
+    return apiInstance.post('/products', data);
+  },
+  updateProduct(id: number, data: Product) {
+    return apiInstance.put(`/products/${id}`, data);
+  },
+  deleteProduct(id: number) {
+    return apiInstance.delete(`/products/${id}`);
+  },
+};
+
+export default productService;
